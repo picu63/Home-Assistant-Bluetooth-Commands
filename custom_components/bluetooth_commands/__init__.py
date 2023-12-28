992d4979e96f3ec4a109f49493baced38e22fbed
Piotr Olearczyk <picu63@gmail.com>
2023-12-28 09:36:21 +0100

    creating bluetooth service

--- custom_components/bluetooth_commands/__init__.py
+++ custom_components/bluetooth_commands/__init__.py
@@ -14,16 +14,23 @@ from __future__ import annotations
 
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
-from homeassistant.components import bluetooth
 
 # The domain of your component. Should be equal to the name of your component.
 DOMAIN = "bluetooth_commands"
 
+ATTR_NAME = "name"
+DEFAULT_NAME = "World"
 
 def setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Set up a skeleton component."""
     # States are in the format DOMAIN.OBJECT_ID.
-    hass.states.set('bluetooth_commands.Hello_World', 'Works!')
+    def handle_hello(call):
+        """Handle the service call."""
+        name = call.data.get(ATTR_NAME, DEFAULT_NAME)
+
+        hass.states.set("bluetooth_commands.hello", name)
+    
+    hass.services.register(DOMAIN, "hello", handle_hello)
 
     # Return boolean to indicate that initialization was successfully.
     return True
